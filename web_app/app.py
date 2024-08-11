40fcf5f9a5c0d8e44e65461cc708fc8eb0cb70f8
Ezekiel Bassey <bazzdude007@gmail.com>
2024-08-11 22:51:16 +0100

    Update app.py

--- web_app/app.py
+++ web_app/app.py
@@ -2,8 +2,28 @@
 from flask import Flask, render_template
 from models import storage
 from models.user import User
+from api.v1.views import app_views
 
 app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"api/v1/*": {"origins": "*"}})
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Close storage"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 Error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+@app.errorhandler(500)
+def server_error(error):
+    return make_response(jsonify({"error": "Something went wrong"}), 500)
 
 
 @app.route("/", strict_slashes=False)
