73f624440e45abc788d1f95aa1ae0a95b491045c
Ezekiel Bassey <bazzude007@gmail.com>
2024-09-27 04:30:20 +0100

    refatored the app structure

---
+++ backend/models/engine/db_storage.py
@@ -0,0 +1,27 @@
+"""Database storage class"""
+
+class DBStorage():
+
+    def new():
+        """creates new object"""
+        pass
+
+    def get(id):
+        """get an object from the database by id"""
+        pass
+
+    def all(class_name=""):
+        """get all objects in the database"""
+        pass
+
+    def save():
+        """save an object to the database"""
+        pass
+
+    def delete(id):
+        """remove an object from the database"""
+        pass
+
+    def create_tables():
+        """create the database tables"""
+        pass
